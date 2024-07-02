d646b8e6e27f466b7edc8fc281cd0e848e0a34d6
berthemoose <berthemoose@gmail.com>
2024-07-02 17:45:33 +0200

    notes

--- 03a_files_async.js
+++ 03a_files_async.js
@@ -7,7 +7,8 @@ const fileOps = async () => {
     try {
         const data = await fsPromises.readFile(path.join(__dirname,'files','starter.txt'), 'utf8');
         console.log(data);
-        await fsPromises.writeFile(path.join(__dirname,'files','promiseWrite.txt'), data)
+        await fsPromises.unlink(path.join(__dirname,'files','starter.txt'));
+        await fsPromises.writeFile(path.join(__dirname,'files','promiseWrite.txt'), data);
         await fsPromises.appendFile(path.join(__dirname,'files','promiseWrite.txt'), '\n\nNice to meet you');
         await fsPromises.rename(path.join(__dirname,'files','promiseWrite.txt'), (path.join(__dirname,'files','promiseComplete.txt')));
         const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf8');
@@ -16,5 +17,5 @@ const fileOps = async () => {
         console.error(err);
     }
 }
-
 fileOps();
+
